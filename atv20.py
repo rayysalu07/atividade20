98ac985a1d7a0e3db13e486c644a77a220197c67
rayysalu07 <148983634+rayysalu07@users.noreply.github.com>
2023-11-01 11:18:15 -0300

    Add files via upload

---
+++ atv20.py
@@ -0,0 +1,17 @@
+# Exercício Python 28: Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5
+# e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+# O programa deverá escrever na tela se o usuário venceu ou perdeu. 
+
+
+import random # importando a blioteca random (gera numeros randomicos)
+
+numeroAleatorio = random.randint(0, 5) # cria numeros candomicos de 0 a 5 (0, 5)
+numeroUsuario = int(input("digite um valor 0 até 5"))
+# comparar os Numeros 
+if numeroUsuario <= 5:
+    if numeroAleatorio == numeroUsuario:
+        print(f"{numeroUsuario} = {numeroAleatorio}")
+    else:
+        print(f"{numeroUsuario} != {numeroAleatorio}")
+else:
+    print("valor invalido!")
